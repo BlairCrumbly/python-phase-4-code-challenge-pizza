7c715783d97b766a8a2e73364a534fe54153fd2a
Blair Crumbly <blaircrumbly@gmail.com>
2025-03-18 03:47:10 -0400

    s.rules and relationships added

--- server/models.py
+++ server/models.py
@@ -21,9 +21,10 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
-
+    restaurant_pizzas = db.relationship("restaurant_pizzas", back_populates="pizza", cascade="all, delete-orphan")
+    pizzas = association_proxy("restaurant_pizzas", "pizza")
     # add serialization rules
-
+    serialize_rules = ("-restaurantpizzas", "-pizzas")
     def __repr__(self):
         return f"<Restaurant {self.name}>"
 
@@ -36,24 +37,32 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
-
+    #!establish bidirectional relationship with RP table + prevent parent deletion errors for orphans
+    #*Resturant needs access to its related pizza objs through RP
+    restaurant_pizzas = db.relationship("restaurant_pizzas", back_populates="restaurant", cascade="all, delete-orphan")
+    #!shortcut to access related Restaurant objects directly through the RestaurantPizza table
+    restaurants = association_proxy("restaurant_pizzas", "resturant")
     # add serialization rules
-
+    #!ALWAYS exclude relationships, circular reference
+    #? could also just take off dot notation?
+    serealize_rules = ("-restaurantpizzas", "-resturants")
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
 
 
 class RestaurantPizza(db.Model, SerializerMixin):
     __tablename__ = "restaurant_pizzas"
-
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
-
+    #!establish join table by foreignkeys
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    restaurant_id  = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
     # add relationships
-
+    restaurant = db.relationship("Restaurant", back_populates = "RestaurantPizza")
+    pizza = db.relationship("Pizza", back_populates="RestaurantPizza")
     # add serialization rules
-
+    serialize_rules = ("-restaurant", "-pizza")
     # add validation
-
+    
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
